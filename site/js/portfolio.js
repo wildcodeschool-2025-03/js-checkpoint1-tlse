c30cc0d761cb4517b01ded84e636e0cf58f3aa59
ArthurHtbk <arthur.heurtebise@gmail.com>
2025-03-31 17:02:19 +0200

    js step 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,7 @@
 const avatar = document.querySelector(".avatar")
 const firstname = document.querySelector("#firstname")
 const modify = document.querySelector(".modify")
+const pinkBg = document.querySelector(".pink-bg")
 
 avatar.addEventListener("click", () => {
     avatar.src = "./image/avatar.svg"
@@ -8,6 +9,8 @@ avatar.addEventListener("click", () => {
 
 modify.addEventListener("click", () => {
     const username = prompt("Hey buddy what's your name?")
+    const userColor = prompt("Hey buddy what's your color?")
     firstname.textContent = username
     firstname.style.color = "white"
+    pinkBg.style.backgroundColor = userColor
 })
